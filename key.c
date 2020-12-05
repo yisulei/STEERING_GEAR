4de1972da5f88097e4d8c534dc1e3945896beb9c
yisulei <75148280+yisulei@users.noreply.github.com>
2020-12-05 16:00:46 +0800

    控制按键

---
+++ key.c
@@ -0,0 +1,20 @@
+//GPIOB,Pin0 控制按键
+#include "sys.h"
+#include "key.h"
+#include "delay.h"
+
+
+void KEY_Init(void)
+{
+	
+	GPIO_InitTypeDef  GPIO_InitStructure;
+
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);//使能GPIOA时钟
+ 
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0; // KEY1 对应引脚
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;//普通输入模式
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;//100M
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;//上拉
+  GPIO_Init(GPIOB, &GPIO_InitStructure);//初始化GPIOE2,3,4
+} 
+
---
+++ key.h
@@ -0,0 +1,9 @@
+#ifndef _KEY_
+#define _KEY_
+#include "sys.h"
+
+void KEY_Init(void);
+
+#endif
+
+
