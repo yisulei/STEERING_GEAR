5b90f37e815b9785cc35b2a948426439f81bc825
yisulei <75148280+yisulei@users.noreply.github.com>
2020-12-05 16:16:12 +0800

    Update main.c

--- main.c
+++ main.c
@@ -9,31 +9,23 @@
 int main(void)
 	
 {
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//ÉèÖÃÏµÍ³ÖĞ¶ÏÓÅÏÈ¼¶·Ö×é2
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//è®¾ç½®ç³»ç»Ÿä¸­æ–­ä¼˜å…ˆçº§åˆ†ç»„2
 	KEY_Init();
-  TIM2_PWM_Init(20000-1,84-1);
+        TIM2_PWM_Init(20000-1,84-1);
 	TIM3_PWM_Init(20000-1,84-1);
 	LED_Init();
-	TIM4_Int_Init(100-1,840-1);//1msÖĞ¶ÏÒ»´Î  84/84M*1000=1ms
-	//TIM_Cmd(TIM4,ENABLE); //Ê¹ÄÜ¶¨Ê±Æ÷3
-	SystemInit();
-	
-	//TIM_SetCompare1(TIM2,975);			
-
+	TIM4_Int_Init(100-1,840-1);//1msä¸­æ–­ä¸€æ¬¡  84/84M*1000=1ms
+	SystemInit()ï¼›			
 	while(1)
 	{
 		while (GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_0)==1)
 		{
-			//TIM4_Int_Init(1000-1,84-1);//1msÖĞ¶ÏÒ»´Î  84/84M*1000=1ms
 			GPIO_SetBits(GPIOC,GPIO_Pin_0);
-			//TIM_Cmd(TIM4,ENABLE); //Ê¹ÄÜ¶¨Ê±Æ÷3
-			TIM_SetCompare1(TIM2,1500);  //(arr+1)-(arr+1)*Õ¼¿Õ±È  ¶æ»ú1×ª90¶È
-		}
-		//TIM_SetCompare1(TIM2,500);
-		//TIM_SetCompare1(TIM3,500);//¶æ»ú1,2¶¼×ªµ½0¶È		
+			TIM_SetCompare1(TIM2,1500);  //(arr+1)-(arr+1)*å ç©ºæ¯”  èˆµæœº1è½¬90åº¦
+		}		
 		GPIO_ResetBits(GPIOC,GPIO_Pin_1);
 		GPIO_ResetBits(GPIOC,GPIO_Pin_0);
-		//TIM_Cmd(TIM4,DISABLE);
+		
   }
 	
 }
